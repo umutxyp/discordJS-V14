2270983a1f8340083ef8043c0b4d159e4b7df5c6
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-22 18:01:52 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,20 +1,22 @@
-const config = require("../config.js")
-const prefix = config.prefix; 
+const { EmbedBuilder } = require("discord.js");
+var config = require("../config.js");
+const client = require("..");
+const prefix = config.prefix;
 
-module.exports = async (client, message) => {
-    if (!message.guild) return;
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-    let command = message.content.split(" ")[0].slice(prefix.length);
-    let params = message.content.split(" ").slice(1);
-    let cmd;
-    if (client.commands.has(command)) {
-      cmd = client.commands.get(command);
-    } else if (client.aliases.has(command)) {
-      cmd = client.commands.get(client.aliases.get(command));
-    }
-    if (cmd) {
-      cmd.run(client, message, params);
-    }
-
+client.on("messageCreate", async (message) => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let params = message.content.split(" ").slice(1);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
   }
+  if (cmd) {
+    cmd.run(client, message, params);
+  }
+
+});
