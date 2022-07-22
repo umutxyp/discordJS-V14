6b22a1544384a13341fc5f9d58ae4e657f226f5d
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-22 17:56:19 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,20 +1,20 @@
-var config = require("../config.js");
-const prefix = config.prefix;
+const config = require("../config.js")
+const prefix = config.prefix; 
 
 module.exports = async (client, message) => {
-  if (!message.guild) return;
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(" ")[0].slice(prefix.length);
-  let params = message.content.split(" ").slice(1);
-  let cmd;
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
-  if (cmd) {
-    cmd.run(client, message, params);
-  }
+    if (!message.guild) return;
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    let command = message.content.split(" ")[0].slice(prefix.length);
+    let params = message.content.split(" ").slice(1);
+    let cmd;
+    if (client.commands.has(command)) {
+      cmd = client.commands.get(command);
+    } else if (client.aliases.has(command)) {
+      cmd = client.commands.get(client.aliases.get(command));
+    }
+    if (cmd) {
+      cmd.run(client, message, params);
+    }
 
-});
+  }
